91e99dc5538a169117d7db8c31a9fc0595f6c762
Erickson Mauricio <erickson.trovata@gmail.com>
2020-01-12 20:54:02 -0300

    # alterações de layout home e login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,5 +1,106 @@
+<style type="text/css">
+	.login-form {
+		/* width: 340px; */
+    	margin: 5px auto;
+	}
+    .login-form form {
+    	margin-bottom: 5px;
+        background: #f7f7f7;
+        box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
+        padding: 30px;
+    }
+    .login-form h2 {
+        margin: 0 0 15px;
+    }
+    .login-form .hint-text {
+		color: #777;
+		padding-bottom: 15px;
+		text-align: center;
+    }
+    .form-control, .btn {
+        min-height: 38px;
+        border-radius: 2px;
+    }
+    .login-btn {        
+        font-size: 15px;
+        font-weight: bold;
+    }
 
+    .btn-font-color {
+        color: white;
+    }
+
+    .or-seperator {
+        margin: 20px 0 10px;
+        text-align: center;
+        border-top: 1px solid #ccc;
+    }
+    .or-seperator i {
+        padding: 0 10px;
+        background: #f7f7f7;
+        position: relative;
+        top: -11px;
+        z-index: 1;
+    }
+    .social-btn .btn {
+        margin: 10px 0;
+        font-size: 15px;
+        text-align: center; 
+        line-height: 24px;  
+        color: #f4f4f4;     
+    }
+	.social-btn .btn i {
+		margin: 4px 5px  0 5px;
+        /* min-width: 15px; */
+	}
+	.input-group-addon .fa{
+		font-size: 18px;
+	}
+</style>
 <div class="modal fade" id="login">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title">Faça seu login ou cadastro</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
+                    <span aria-hidden="true">&times;</span>
+                </button>                
+            </div>
+
+            <div class="modal-body">
+                <div class="login-form">
+                    <form method="POST" action="{{ route('login') }}">
+						@csrf                    
+                        <div class="form-group">
+                            <div class="input-group">
+                                <span class="input-group-addon"><i class="fa fa-user"></i></span>
+                                <input type="text" style="margin-left: 5px;" class="form-control" name="username" placeholder="E-mail" required="required">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="input-group">
+                                <span class="input-group-addon"><i class="fa fa-lock"></i></span>
+                                <input type="password" style="margin-left: 5px;" class="form-control" name="password" placeholder="Senha" required="required">
+                            </div>
+                        </div>        
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-success btn-block login-btn">{{ __('Login') }}</button>                            
+                        </div>
+                        
+                        <div class="or-seperator"><i>ou</i></div>
+                        
+                        <div class="text-center social-btn">
+                            <a href="{{ url('/auth/redirect/facebook') }}" class="btn btn-primary btn-block btn-font-color"><i class="fab fa-facebook"></i> Acessar com <b>Facebook</b></a>                           
+                            <a href="{{ url('/auth/redirect/google') }}" class="btn btn-danger btn-block"><i class="fab fa-google"></i> Acessar com <b>Google</b></a>
+                            <a href="{{ route('register') }}" class="btn btn-info btn-block"><i class="fas fa-envelope"></i> Cadastro por e-mail</a>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- <div class="modal fade" id="login">
 	<div class="modal-dialog" role="document">
 		<div class="modal-content">
 			<div class="modal-header modal-header-login">
@@ -32,7 +133,7 @@
 									<span class="invalid-feedback" role="alert">
 										<strong>{{ $message }}</strong>
 									</span>
-								@enderror
+								@enderror -->
 
 						<!-- <div class="form-group row">
 							<div class="col-md-6 offset-md-4">
@@ -45,7 +146,7 @@
 								</div>
 							</div>
 						</div> -->
-
+<!-- 
 						<div class="form-group row mb-0 login">
 							<div class="links">
 								@if (Route::has('password.request'))
@@ -55,9 +156,7 @@
 								@endif
 							</div>
 							<div class="entrar">
-								<button type="submit" class="btn btn-primary">
-									{{ __('Login') }}
-								</button>
+								<button type="submit" class="btn btn-primary">{{ __('Login') }}</button>
 							</div>
 						</div>
 					</form>
@@ -76,4 +175,4 @@
 			</div>
 		</div>
 	</div>
-</div>
+</div> -->
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,31 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Sheep Haus') }}</title>
+
+    <!-- Favicons -->
+    <link href="img/favicon.png" rel="icon">
+    <link href="img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Montserrat:300,400,500,700" rel="stylesheet">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}" />
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+        integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <style>
+        .field-icon {
+            float: right;
+            margin-left: -30px;
+            margin-top: -25px;
+            position: relative;
+            z-index: 2;
+        }
+    </style>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -25,112 +49,77 @@
 </head>
 <body>
 	@include('../auth.login')
-    <div id="app">
-        <nav class="menu">
-            <div class="centralizar">
-				<header class="menu--topo">
-					<a class="navbar-brand" href="{{ url('/') }}">
-	                    {{ config('app.name', 'Laravel') }}
-	                </a>
 
+    <header id="header">
+        <div class="container-fluid" style="background: rgb(169, 7, 7);">
 
-					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-	                    <span class="navbar-toggler-icon"></span>
-	                </button>
-
-	                <section class="menu--itens" id="navbarSupportedContent">
-	                    <!-- Left Side Of Navbar -->
-	                    <ul class="menu--itens_lista">
-							<li class="lista-item">
-								<p>Tenho imóvel</p>
-								<ul class="sub_menu--itens_lista">
-									<li class="sub-lista-item">
-										<a href="{{route('home')}}">Divulgar</a>
-									</li>
-									<li class="sub-lista-item">
-										<a href="{{route('home')}}">Match moradores</a>
-									</li>
-								</ul>
-							</li>
-							<li class="lista-item">
-								<p>Sou aluno</p>
-								<ul class="sub_menu--itens_lista">
-									<li class="sub-lista-item">
-										<a href="{{route('home')}}">Match imóveis</a>
-									</li>
-									<li class="sub-lista-item">
-										<a href="{{route('home')}}">Amigos</a>
-									</li>
-								</ul>
-							</li>
-							<li class="lista-item">
-								<p>Prestador Serviços</p>
-								<ul class="sub_menu--itens_lista">
-									<li class="sub-lista-item">
-										<a href="{{route('home')}}">Anunciar</a>
-									</li>
-								</ul>
-							</li>
-							<li class="lista-item">
-								<a href="{{route('home')}}">Procurar vaga</a>
-							</li>
-							<li class="lista-item">
-								<a href="{{route('home')}}">Anunciar Serviços</a>
-							</li>
-							<li class="lista-item">
-								<a href="{{route('home')}}">Anunciar vaga</a>
-							</li>
-							@guest
-							   <li class="lista-item">
-								   <a data-toggle="modal" data-target="#login"class="login" href="{{ route('login') }}">{{ __('Login') }}</a>
-							   </li>
-							   @if (Route::has('register'))
-								   <!-- <li class="lista-item">
-									   <a class="registro" href="{{ route('register') }}">{{ __('Register') }}</a>
-								   </li> -->
-							   @endif
-						   @else
-							   <li class="lista-item dropdown">
-								   <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-									   {{ Auth::user()->name }} <span class="caret"></span>
-								   </a>
-
-								   <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-										 
-										<a href="{{ route('dashboard') }}" class="dropdown-item">Dashboard</a>
-										<a href="{{ route('profile') }}" class="dropdown-item">Profile</a>
-
-											<a class="dropdown-item" href="{{ route('logout') }}"
-										  onclick="event.preventDefault();
-														document.getElementById('logout-form').submit();">
-										   {{ __('Logout') }}
-									   </a>
-
-									   <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-										   @csrf
-										 </form>
-										 
-										 
-								   </div>
-							   </li>
-						   @endguest
-						   <li class="lista-item menu--linguagem">
-   							<div class="dropdown">
-   								<div class="botao-dropdown">BR</div>
-   								<input type="checkbox" id="dropdown">
-   								<ul class="dropdown-menu">
-   									<li><a href="{{Request::url()}}?locale=en">BR</a></li>
-   									<li><a href="{{Request::url()}}?locale=pt-BR">EN</a></li>
-   									<li><a href="{{Request::url()}}?locale=es">ES</a></li>
-   								</ul>
-   							</div>
-   						</li>
-	                    </ul>
-	                </section>
-				</header>
+            <div id="logo" class="pull-left">
+                <h1><a class="navbar-brand" href="{{ url('/') }}">{{ config('app.name', 'Sheep Haus') }}</a></h1>
             </div>
-        </nav>
 
+            <nav id="nav-menu-container">
+                <ul class="nav-menu">
+                    <li class="nav-item dropdown"><a href="#">Tenho imóvel</a>
+                        <ul class="dropdown-menu">
+                            <li class="dropdown-item"><a href="{{route('home')}}">Divulgar</a></li>
+                            <li class="dropdown-item"><a href="{{route('home')}}">Match moradores</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">Sou aluno</a>
+                        <ul>
+                            <li><a href="{{route('home')}}">Match imóveis</a></li>
+                            <li><a href="{{route('home')}}">Amigos</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">Prestador Serviços</a>
+                        <ul>
+                            <li><a href="{{route('home')}}">Anunciar</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="{{route('home')}}">Procurar vaga</a>
+                    </li>
+                    <li>
+                        <a href="{{route('home')}}">Anunciar Serviços</a>
+                    </li>
+                    <li>
+                        <a href="{{route('home')}}">Anunciar vaga</a>
+                    </li>
+                    <li><a href="#">BR</a>
+                        <ul>
+                            <li><a href="{{Request::url()}}?locale=en">BR</a></li>
+                            <li><a href="{{Request::url()}}?locale=pt-BR">EN</a></li>
+                            <li><a href="{{Request::url()}}?locale=es">ES</a></li>
+                        </ul>
+                    </li>
+                    @guest
+                        <li class="menu-active">
+                            <button class="btn btn-danger" data-toggle="modal" data-target="#login">{{ __('Login') }}</button>                            
+                        </li>
+                        @if (Route::has('register'))
+                            <!-- <li class="lista-item">
+                                <a class="registro" href="{{ route('register') }}">{{ __('Register') }}</a>
+                            </li> -->
+                        @endif
+                    @else
+                        <li><a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>{{ Auth::user()->name }} <span class="caret"></span></a>
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">                                    
+                                <a href="{{ route('dashboard') }}" class="dropdown-item">Dashboard</a>
+                                <a href="{{ route('profile') }}" class="dropdown-item">Profile</a>
+                                    <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">{{ __('Logout') }}</a>
+
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    @csrf
+                                </form>                                                                        
+                            </div>
+                        </li>
+                    @endguest                    
+                </ul>
+            </nav><!-- #nav-menu-container -->
+        </div>
+    </header><!-- #header -->
+
+    <div id="app">
         <main>
             @yield('content')
         </main>
@@ -139,7 +128,6 @@
 				<nav class="menu--rodape">
 					<section class="menu--linguagem" id="navbarSupportedContent">
 						<a class="logo" href="{{ url('/') }}">
-						<!-- {{ config('app.name', 'Laravel') }} -->
 							<figure>
 								<img src="images/icones/logo_completa.png" alt="">
 							</figure>
