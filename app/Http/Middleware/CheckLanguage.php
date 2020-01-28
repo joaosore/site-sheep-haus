392fb77a30a363d406b06e86691a74c93f574c90
Thiago Andrade <thiagoandrade@MacBook-Pro-de-Thiago.local>
2020-01-28 01:15:17 -0300

    Verifica o idioma do browser caso não tenha cookie nem parametro

--- app/Http/Middleware/CheckLanguage.php
+++ app/Http/Middleware/CheckLanguage.php
@@ -16,16 +16,19 @@ class CheckLanguage
     public function handle($request, Closure $next)
     {
         $URLlocale = $request->input('locale');
+        $COOKIElocale = $_COOKIE["user_locale"];
+        $BROWSERlocale = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
 
         if ($URLlocale) {
             \App::setLocale($URLlocale);
-            setcookie("user_locale", $URLlocale, time()+3600*100);  /* expira em 100 horas */
-        } else {
-            $COOKIElocale = $_COOKIE["user_locale"];
-            if ($COOKIElocale) {
-                \App::setLocale($COOKIElocale);
-            }
-        }
+            setcookie("user_locale", $URLlocale, time()+3600*100);
+            
+        } else if ($COOKIElocale) {
+            \App::setLocale($COOKIElocale);
+
+        } else if ($BROWSERlocale) {
+            \App::setLocale($BROWSERlocale);
+        } 
 
         return $next($request);
     }
